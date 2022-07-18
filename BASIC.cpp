66196b35f329822de327739ac5f90cfba6db6feb
Harshit Verma <96187405+Harshitv21@users.noreply.github.com>
2022-07-18 19:24:25 +0530

    Added #define & const before main

--- BASIC.cpp
+++ BASIC.cpp
@@ -7,6 +7,15 @@ Date:
 
 #include <bits/stdc++.h>
 
+// #define ar array
+#define ll long long
+#define ld long double
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
 int main() 
 {
     int i, t = 0;
