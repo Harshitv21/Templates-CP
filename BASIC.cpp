5474bc9e65c3a763a0ce8ee1fb8b62fbe8d767f0
Harshit Verma <96187405+Harshitv21@users.noreply.github.com>
2022-07-19 07:49:39 +0530

    Added sync_with_stdio, cin.tie, cout.tie (in comment state)

--- BASIC.cpp
+++ BASIC.cpp
@@ -18,8 +18,14 @@ const ld EPS = 1e-9;
 
 int main() 
 {
+    /* 
+    ios_base::sync_with_stdio(false); [https://stackoverflow.com/questions/31162367/significance-of-ios-basesync-with-stdiofalse-cin-tienull]
+    std::cin.tie(0);
+    std::cout.tie(0);
+    */
+
     int t = 0;
-    
+
     std::cin >> t;
     for(int i = 1; i <= t; i++)
     {
